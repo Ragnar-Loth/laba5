1d93ba6a8b3574d56705c26b6ba0190fb7fe93bb
Prystay Oleksandr <prystay13579@gmail.com>
2019-12-03 23:18:54 +0200

    correction file

--- Source.cpp
+++ Source.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 class Matrix {
 private:
-	int matrix[5];
+	int matr1x[5];
 public:
-	void InputElement(int a) {
-		cin >> mtx[a];
+	void inputelement(int a) {
+		cin >> matr1x[a];
 	}
 	void sMatrix(int a) {
-		static int ptr = 0;
-		mtx[ptr] = a;
-		if (ptr == 4) ptr = -1;
-		ptr++;
+		static int arraysize = 0;
+		matr1x[arraysize] = a;
+		if (arraysize == 4) arraysize = -1;
+		arraysize++;
 	}
 
-	int* GetMatrix() {
-		return mtx;
+	int* getmatrix() {
+		return matr1x;
 	}
 
-	friend void OutputMatrix(Matrix* A, int n);
+	friend void outputMatrix(Matrix* A, int n);
 
 
 	void merge(int start, int mid, int end) {
@@ -27,31 +27,31 @@ public:
 		int i = start, j = mid + 1, k = 0;
 
 		while (i <= mid && j <= end) {
-			if (this->mtx[i] <= this->mtx[j]) {
-				t[k] = this->mtx[i];
+			if (this->matr1x[i] <= this->matr1x[j]) {
+				t[k] = this->matr1x[i];
 				k++;
 				i++;
 			}
 			else {
-				t[k] = this->mtx[j];
+				t[k] = this->matr1x[j];
 				k++;
 				j++;
 			}
 		}
 
 		while (i <= mid) {
-			t[k] = this->mtx[i];
+			t[k] = this->matr1x[i];
 			k++;
 			i++;
 		}
 
 		while (j <= end) {
-			t[k] = this->mtx[j];
+			t[k] = this->matr1x[j];
 			k++;
 			j++;
 		}
 		for (i = start; i <= end; i++) {
-			this->mtx[i] = t[i - start];
+			this->matr1x[i] = t[i - start];
 		}
 	}
 
@@ -67,10 +67,10 @@ public:
 	}
 };
 
-void OutputMatrix(Matrix* mtx, int n) {
+void outputMatrix(Matrix* matr1x, int n) {
 	for (int i = 0; i < n; ++i)
 	{
-		cout << mtx->mtx[i];
+		cout << matr1x->matr1x[i];
 	}
 	cout << endl;
 }
@@ -80,31 +80,31 @@ int main() {
 	Matrix matrix[5];
 	for (int i = 0; i < 5; i++)
 		for (int j = 0; j < 5; j++) {
-			matrix[j].InputElement(i);
+			matrix[j].inputelement(i);
 		}
 	Matrix matrix1;
 	for (int i = 0; i < 5; i++)
 		matrix[i].mergeSort(0, 4);
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
-			cout << matrix[j].GetMatrix()[i] << "\t ";
-			matrix1.sMatrix(matrix[j].GetMatrix()[i]);
+			cout << matrix[j].getmatrix()[i] << "\t ";
+			matrix1.sMatrix(matrix[j].getmatrix()[i]);
 		}
 		cout << endl;
 	}
 	double sum = 0;
 	int f1 = 1, f2 = 1, f3 = 1, f4 = 1;
 	for (int i = 0; i < 4; i++) {                                  // f1
-		f1 *= matrix[i].GetMatrix()[0];
+		f1 *= matrix[i].getmatrix()[0];
 	}
 	for (int i = 0; i < 3; i++) {                                  // f1
-		f2 *= matrix[i].GetMatrix()[1];
+		f2 *= matrix[i].getmatrix()[1];
 	}
 	for (int i = 0; i < 2; i++) {                                  // f1
-		f3 *= matrix[i].GetMatrix()[2];
+		f3 *= matrix[i].getmatrix()[2];
 	}
 	for (int i = 0; i < 1; i++) {                                  // f1
-		f4 *= matrix[i].GetMatrix()[3];
+		f4 *= matrix[i].getmatrix()[3];
 	}
 	cout << "\n";
 	sum = (f1 + f2 + f3 + f4);
